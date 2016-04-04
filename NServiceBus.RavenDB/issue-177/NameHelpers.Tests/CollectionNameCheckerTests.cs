3a582a310dc59454abe8e6717503640a2f34a560
Jan Ove Skogheim <janovesk@janovesk.com>
2016-04-04 22:33:06 +0200

    Added new duplicate collection name checker

---
+++ NServiceBus.RavenDB/issue-177/NameHelpers.Tests/CollectionNameCheckerTests.cs
@@ -0,0 +1,80 @@
+﻿using NUnit.Framework;
+
+namespace NameHelpers.Tests
+{
+    [TestFixture]
+    public class CollectionNameCheckerTests
+    {
+        [Test]
+        public void OrderSagaDatasShouldMatchOrderSaga() //saga name: OrderSagaData
+        {
+            var collections = new[] {"OrderSaga", "OrderSagaDatas"};
+            string match;
+            Assert.IsTrue(CollectionNameChecker.CheckForMatch("OrderSagaDatas", collections, out match));
+            Assert.AreEqual("OrderSaga", match);
+        }
+
+        [Test]
+        public void OrderSagasShouldMatchOrderSaga()//saga name: OrderSaga
+        {
+            var collections = new[] { "OrderSaga", "OrderSagas" };
+            string match;
+            Assert.IsTrue(CollectionNameChecker.CheckForMatch("OrderSagas", collections, out match));
+            Assert.AreEqual("OrderSaga", match);
+        }
+
+        [Test]
+        public void DataOrderSagaDatasShouldMatchOrderSaga()//saga name: DataOrderSagaData
+        {
+            var collections = new[] { "OrderSaga", "DataOrderSagaDatas" };
+            string match;
+            Assert.IsTrue(CollectionNameChecker.CheckForMatch("DataOrderSagaDatas", collections, out match));
+            Assert.AreEqual("OrderSaga", match);
+        }
+
+        [Test]
+        public void BlueBerriesShouldMatchBlueBerry()//saga name: Blueberry
+        {
+            var collections = new[] { "BlueBerry", "BlueBerries" };
+            string match;
+            Assert.IsTrue(CollectionNameChecker.CheckForMatch("BlueBerries", collections, out match));
+            Assert.AreEqual("BlueBerry", match);
+        }
+
+        [Test]
+        public void CarsShouldMatchCar()//saga name: Car
+        {
+            var collections = new[] { "Car", "Cars" };
+            string match;
+            Assert.IsTrue(CollectionNameChecker.CheckForMatch("Cars", collections, out match));
+            Assert.AreEqual("Car", match);
+        }
+
+        [Test]
+        public void OrderDataIdentitiesShouldMatchOrderIdentity()//saga name: OrderDataIdentity
+        {
+            var collections = new[] { "OrderIdentity", "OrderDataIdentities" };
+            string match;
+            Assert.IsTrue(CollectionNameChecker.CheckForMatch("OrderDataIdentities", collections, out match));
+            Assert.AreEqual("OrderIdentity", match);
+        }
+
+        [Test]
+        public void OrderDataPolicyDatasShouldMatchOrderPolicy()//saga name: OrderDataPolicyData
+        {
+            var collections = new[] { "OrderPolicy", "OrderDataPolicyDatas" };
+            string match;
+            Assert.IsTrue(CollectionNameChecker.CheckForMatch("OrderDataPolicyDatas", collections, out match));
+            Assert.AreEqual("OrderPolicy", match);
+        }
+
+        [Test]
+        public void DataofDatasPolicyDataDatasDatasShouldMatchofsPolicys()//saga name: DataofDatasPolicyDataDatasData
+        {
+            var collections = new[] { "ofsPolicys", "DataofDatasPolicyDataDatasDatas" };
+            string match;
+            Assert.IsTrue(CollectionNameChecker.CheckForMatch("DataofDatasPolicyDataDatasDatas", collections, out match));
+            Assert.AreEqual("ofsPolicys", match);
+        }
+    }
+}
---
+++ NServiceBus.RavenDB/issue-177/NameHelpers/CollectionNameChecker.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameHelpers
+{
+    public static class CollectionNameChecker
+    {
+        public static bool CheckForMatch(string candidate, string[] collections, out string match)
+        {
+            if (candidate.EndsWith("Datas"))
+            {
+                var candidateWithoutEndingDatasAndAnyOtherData = candidate.Substring(0, candidate.LastIndexOf("Datas")).Replace("Data", string.Empty);
+
+                if (collections.Contains(candidateWithoutEndingDatasAndAnyOtherData))
+                {
+                    match = candidateWithoutEndingDatasAndAnyOtherData;
+                    return true;
+                }
+
+                match = null;
+                return false;
+            }
+
+            var candidateWithoutData = candidate.Replace("Data", string.Empty);
+            foreach (var collection in collections)
+            {
+                var pluralizedCollection = Raven.Client.Util.Inflector.Pluralize(collection);
+                if (candidateWithoutData == pluralizedCollection)
+                {
+                    match = collection;
+                    return true;
+                }
+            }
+
+            match = null;
+            return false;
+        }
+    }
+
+
+}
