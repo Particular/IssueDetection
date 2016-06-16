4dacebab4a3971023286a1b1c120c16a3d528f1c
Szymon Kulec <scooletz@gmail.com>
2016-06-16 17:18:47 +0200

    Initial test setup.

---
+++ NServiceBus.AzureStoragePersistence/issue-74/IndexPruner.Tests/Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.WindowsAzure.Storage;
+using NServiceBus.SagaPersisters.Azure;
+using NUnit.Framework;
+
+namespace IndexPruner.Tests
+{
+    public class Tests
+    {
+        private readonly CloudStorageAccount _account;
+        private AzureSagaPersister persister;
+
+        public Tests()
+        {
+            _account =
+                CloudStorageAccount.Parse(Environment.GetEnvironmentVariable("AzureStoragePersistence.ConnectionString"));
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            persister = new AzureSagaPersister(_account, true);
+        }
+    }
+}
