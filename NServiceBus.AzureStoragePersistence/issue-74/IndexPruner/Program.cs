a85c8121c23191eb9d7cf757aa23064054097701
Donald Belcham <donald.belcham@particular.net>
2016-06-21 10:40:47 -0600

    Update logic, naming and output messages

--- NServiceBus.AzureStoragePersistence/issue-74/IndexPruner/Program.cs
+++ NServiceBus.AzureStoragePersistence/issue-74/IndexPruner/Program.cs
@@ -23,39 +23,60 @@ namespace IndexPruner
 
         static void Main(string[] args)
         {
-            MainAsync(args).Wait();
+            MainAsync(args).GetAwaiter().GetResult();
+            Console.WriteLine("Completed. Press any key to exit.");
+            Console.ReadKey();
         }
 
         public static async Task MainAsync(string[] args)
         {
-            var sagaTypes = FindAllSagaTypes();
-            if (sagaTypes.Length == 0)
+            var sagaDataTypes = FindAllSagaDataTypes();
+            if (sagaDataTypes.Length == 0)
             {
                 Console.WriteLine(
-                    "No Saga types found! Did you put all the dlls with sagas into the same directory this executable resides?");
+                    "No Saga Data Types found! Did you put all the dlls with sagas into the same directory this executable resides?");
                 return;
             }
 
-            var connectionString = ConfigurationManager.ConnectionStrings[ConnectionStringName]?.ConnectionString ??
-                                   FetchConnectionString(args);
+            var connectionString = GetConnectionString(args);
             if (string.IsNullOrWhiteSpace(connectionString))
             {
                 Console.WriteLine(
-                    $"Provide a connection string in the standard 'connectionStrings' App.config section with following name: '{ConnectionStringName}'");
+                    $"Provide a connection string in the standard 'connectionStrings' App.config section with following name: '{ConnectionStringName}' \r\nAlternatively, provide a connection string as a commandline parameter when running this tool.");
                 return;
             }
 
-            foreach (var sagaType in sagaTypes)
+            foreach (var sagaDataType in sagaDataTypes)
             {
-                Console.WriteLine($"Prunning indexes of the following saga: {sagaType}");
-                var table = GetTable(connectionString, sagaType.Name);
+                if (sagaDataType.IsGenericType)
+                {
+                    Console.WriteLine($"The saga data type `{sagaDataType.FullName}` is a generic class type and cannot be processed");
+                    continue;
+                }
+
+                Console.WriteLine($"Pruning indexes of the following saga data type: {sagaDataType}");
+                var table = GetTable(connectionString, sagaDataType.Name);
 
                 TableContinuationToken token = null;
                 do
                 {
-                    var segment = await table
-                        .ExecuteQuerySegmentedAsync(new TableQuery<SecondaryIndexTableEntity>(), token)
-                        .ConfigureAwait(false);
+                    TableQuerySegment<SecondaryIndexTableEntity> segment;
+                    try
+                    {
+                        segment = await table
+                            .ExecuteQuerySegmentedAsync(new TableQuery<SecondaryIndexTableEntity>(), token)
+                            .ConfigureAwait(false);
+                    }
+                    catch (StorageException e)
+                    {
+                        if (e.RequestInformation.HttpStatusCode == 404)
+                        {
+                            Console.WriteLine($"Unable to find saga storage table for saga data type `{sagaDataType.FullName}`. Check that your connection string is correct for the sagas data types being scanned.");
+                        }
+                        Console.WriteLine("Error occurred:");
+                        Console.WriteLine(e.ToString());
+                        return;
+                    }
 
                     token = segment.ContinuationToken;
 
@@ -68,6 +89,7 @@ namespace IndexPruner
                             if (deletes.Count > 32)
                             {
                                 await FlushDeletes(deletes).ConfigureAwait(false);
+                                deletes.Clear();
                             }
                         }
                     }
@@ -77,6 +99,17 @@ namespace IndexPruner
             }
         }
 
+        private static string GetConnectionString(string[] args)
+        {
+            var conn = ConfigurationManager.ConnectionStrings[ConnectionStringName]?.ConnectionString;
+            if (string.IsNullOrWhiteSpace(conn))
+            {
+                conn = ParseConnectionStringFromCommandlineArgs(args);
+            }
+
+            return conn;
+        }
+
         private static async Task FlushDeletes(List<Task<ITableEntity>> deletes)
         {
             var entities = await Task.WhenAll(deletes.ToArray());
@@ -84,27 +117,32 @@ namespace IndexPruner
 
             foreach (var failure in failures)
             {
-                Console.WriteLine("Following secondary indexes' entities couldn't be removed:");
+                Console.WriteLine("The following secondary index entities couldn't be removed:");
                 Console.WriteLine($"* {failure.PartitionKey}/{failure.RowKey}");
             }
         }
 
         private static bool IsSecondaryIndex(SecondaryIndexTableEntity entity)
         {
-            Guid g;
-            var hasGuidPartitionKey = Guid.TryParse(entity.PartitionKey, out g);
+            var isSecondaryIndex = entity.PartitionKey.StartsWith("Index_");
             var hasSagaIdSet = entity.SagaId != Guid.Empty;
-            return hasSagaIdSet && !hasGuidPartitionKey;
+            return isSecondaryIndex && hasSagaIdSet;
         }
 
-        private static string FetchConnectionString(string[] args)
+        private static string ParseConnectionStringFromCommandlineArgs(string[] args)
         {
+            if (args.Length != 1)
+            {
+                return null;
+            }
+
             CloudStorageAccount storageAccount;
-            var first = args?[0];
+            var first = args[0];
+            
             return CloudStorageAccount.TryParse(first, out storageAccount) ? first : null;
         }
 
-        private static Type[] FindAllSagaTypes()
+        private static Type[] FindAllSagaDataTypes()
         {
             var scanner = new AssemblyScanner
             {
